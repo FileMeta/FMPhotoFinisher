7ea686f1f30d9f023b6a190720574ffc5b97f2d4
Brandt Redd <bredd@users.noreply.github.com>
2019-07-10 19:40:28 -0600

    Add Deploy.bat

---
+++ Deploy.bat
@@ -0,0 +1,3 @@
+copy "C:\Users\brand\source\FileMeta\FMPhotoFinisher\bin\Release\FMPhotoFinish.exe" "C:\Users\brand\OneDrive\CommandLine"
+copy "C:\Users\brand\source\FileMeta\FMPhotoFinisher\bin\Release\FMPhotoFinish.exe" "\\akershus\Archive\Installs\FMPhotoFinisher"
+ 
